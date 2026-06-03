d9c050273142ca5830f8542881898a8f1d677b66
RupalGangrade <rupalgangrade12@gmail.com>
2026-06-03 20:19:51 +0530

    fixed spacy download

--- models/nlp_models.py
+++ models/nlp_models.py
@@ -1,17 +1,30 @@
+# import spacy
+# from transformers import pipeline
+# import nltk
+
+# nltk.download("punkt")
+
+# try:
+#     nlp = spacy.load("en_core_web_sm")
+# except:
+#     import os
+#     os.system("python -m spacy download en_core_web_sm")
+#     nlp = spacy.load("en_core_web_sm")
+
+# # Lightweight summarization model
+# summarizer = pipeline(
+#     "summarization",
+#     model="sshleifer/distilbart-cnn-12-6"
+# )
+
 import spacy
 from transformers import pipeline
 import nltk
 
 nltk.download("punkt")
 
-try:
-    nlp = spacy.load("en_core_web_sm")
-except:
-    import os
-    os.system("python -m spacy download en_core_web_sm")
-    nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_sm")
 
-# Lightweight summarization model
 summarizer = pipeline(
     "summarization",
     model="sshleifer/distilbart-cnn-12-6"
